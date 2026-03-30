0252bb0b40b5f4fcbd938dbfa7d28038a8230a21
xybayue-tbk <xybayue@gmail.com>
2026-03-30 21:31:12 +0700

    Function Cek Prima

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -6,3 +6,17 @@ using namespace std;
 // =======================
 int angka = 0;
 int pilihan = -1;
+
+// =======================
+// Function Cek Prima
+// =======================
+bool isPrima(int n) {
+    if (n <= 1) return false;
+
+    int i = 2;
+    while (i <= n / 2) {
+        if (n % i == 0) return false;
+        i++;
+    }
+    return true;
+}
